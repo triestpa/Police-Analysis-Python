e81d1afb44890f6e1b0b34fc252ef77c5e4e7381
triestpa <patrick.triest@gmail.com>
2017-10-17 19:30:49 +0700

    Add data download script

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+mkdir data
+cd data
+wget https://stacks.stanford.edu/file/druid:py883nd2578/VT-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/MA-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/WI-clean.csv.gz
