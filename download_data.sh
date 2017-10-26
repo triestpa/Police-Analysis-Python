c72ecb72a94b344b979195452e74c883b6aa761e
triestpa <patrick.triest@gmail.com>
2017-10-26 16:27:20 +0700

    Download data more states in "download_data" script

--- download_data.sh
+++ download_data.sh
@@ -2,4 +2,10 @@ mkdir data
 cd data
 wget https://stacks.stanford.edu/file/druid:py883nd2578/VT-clean.csv.gz
 wget https://stacks.stanford.edu/file/druid:py883nd2578/MA-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/CT-clean.csv.gz
 wget https://stacks.stanford.edu/file/druid:py883nd2578/WI-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/AZ-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/CO-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/NC-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/WA-clean.csv.gz
+wget https://stacks.stanford.edu/file/druid:py883nd2578/TX-clean.csv.gz
